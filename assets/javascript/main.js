25bcb5f46cfc88447094a1a0552d844d19538715
Bradley Jensen <bradley13@gmail.com>
2019-02-10 13:23:37 -0500

    sign in functionality added and working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,19 +8,6 @@ var config = {
   messagingSenderId: "505311407739"
 };
 
-firebase.initializeApp(config);
-var db = firebase.firestore();
-var trainsRef = db.collection(`trains`);
-var now;
-
-$(function() {
-  $(`#add-train-btn`).click(onAddTrainBtn);
-
-  trainsRef.onSnapshot(updateCurrentTrainSchedule);
-
-  setInterval(updateCurrentTrainSchedule, 60000);
-});
-
 var updateCurrentTrainSchedule = function() {
   now = moment();
   $(`#current-time-display`).text(`Current Time: ${now.format(`HH:mm A`)}`);
@@ -76,23 +63,28 @@ var addTrainToDb = function(train) {
 };
 
 var onAddTrainBtn = function() {
-  $(`#add-train-message`).empty();
-  let myTrain = validateTrainForm();
-  if (myTrain.valid) {
-    $(`#add-train-message`).append($(`<h6>`).text(`Train Inputs are Valid`));
-    // add the train to the database
-    addTrainToDb(myTrain);
-
-    $(`#train-name-input`).val(``);
-    $(`#destination-input`).val(``);
-    $(`#first-train-hours`).val(``);
-    $(`#first-train-minutes`).val(``);
-    $(`#frequency-input`).val(``);
+  if(isUserSignedIn()) {
+    $(`#add-train-message`).empty();
+    let myTrain = validateTrainForm();
+    if (myTrain.valid) {
+      $(`#add-train-message`).append($(`<h6>`).text(`Train Inputs are Valid`));
+      // add the train to the database
+      addTrainToDb(myTrain);
+  
+      $(`#train-name-input`).val(``);
+      $(`#destination-input`).val(``);
+      $(`#first-train-hours`).val(``);
+      $(`#first-train-minutes`).val(``);
+      $(`#frequency-input`).val(``);
+    } else {
+      $(`#add-train-message`).append(
+        $(`<h6 class="text-danger">`).text(`One or more inputs are not valid`)
+      );
+    }
   } else {
-    $(`#add-train-message`).append(
-      $(`<h6 class="text-danger">`).text(`One or more inputs are not valid`)
-    );
+    alert("You must be signed in to add a train to the database.");
   }
+
 };
 
 var validateTrainForm = function() {
@@ -188,3 +180,129 @@ var validateFrequency = function(frequency) {
     return true;
   }
 };
+
+// ====================================== //
+// firebase authorization stuff goes here //
+
+// signs into the train scheduler
+function signIn() {
+  // Sign in Firebase with credential from the Google user.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+}
+
+// Signs-out of train scheduler
+function signOut() {
+  // Sign out of Firebase.
+  firebase.auth().signOut();
+}
+
+// Initiate firebase auth.
+function initFirebaseAuth() {
+  // Initialize Firebase.
+  firebase.auth().onAuthStateChanged(authStateObserver);
+}
+
+// Returns the signed-in user's profile Pic URL.
+function getProfilePicUrl() {
+  // Return the user's profile pic URL.
+  return (
+    firebase.auth().currentUser.photoURL || `/assets/images/profile_placeholder.png`
+  );
+}
+
+// Returns the signed-in user's display name.
+function getUserName() {
+  // Return the user's display name.
+  return firebase.auth().currentUser.displayName;
+}
+
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+  // return true if a user is signed-in.
+  return !!firebase.auth().currentUser;
+}
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  if (user) {
+    // User is signed in!
+    // Get the signed-in user's profile pic and name.
+    var profilePicUrl = getProfilePicUrl();
+    var userName = getUserName();
+
+    // Set the user's profile pic and name.
+    $(userPicElement).attr("src", profilePicUrl);
+    $(userNameElement).text(userName);
+
+    // Show user's profile and sign-out button.
+    $(userNameElement).removeAttr("hidden");
+    $(userPicElement).removeAttr("hidden");
+    $(signOutBtn).removeAttr("hidden");
+    $(signOutBtn).removeAttr("disabled");
+
+    // Hide sign-in button.
+    $(signInBtn).attr("hidden", "true");
+    $(signInBtn).attr("disabled", "true");
+
+    // show and enable the add trains element
+    $(addTrainsElement).removeAttr("hidden");
+    // enable the add trains link in the nav
+    $(addTrainsLinkElement).removeAttr("hidden");
+
+
+  } else {
+    // User is signed out!
+    // Hide user's profile.
+    $(userNameElement).attr("hidden", "true");
+    $(userPicElement).attr("hidden", "true");
+
+    // show the sign in button
+    $(signInBtn).removeAttr("hidden");
+    $(signInBtn).removeAttr("disabled");
+
+    // Hide sign-out button.
+    $(signOutBtn).attr("hidden", "true");
+    $(signOutBtn).attr("disabled", "true");
+
+    // hide the add trains element
+    $(addTrainsElement).attr("hidden", "true");
+    // disable the add trains link in the nav
+    $(addTrainsLinkElement).attr("hidden", "true");
+  }
+}
+
+// code execution below this point (loads last)
+
+firebase.initializeApp(config);
+initFirebaseAuth();
+
+var db = firebase.firestore();
+var trainsRef = db.collection(`trains`);
+var now;
+
+// create shortcuts to DOM elements
+var userPicElement = $("#user-pic");
+var userNameElement = $("#user-name");
+var addTrainsLinkElement = $("#add-trains-link");
+var signInBtn = $("#sign-in-btn");
+var signOutBtn = $("#sign-out-btn");
+var currentTimeElement = $("#current-time-display");
+var currentTrainsElement = $("#current-train-schedule");
+var addTrainsElement = $("#add-trains");
+var trainNameInput = $("#train-name-input");
+var destinationInput = $("#destination-input");
+var firstTrainHoursInput = $("#first-train-hours");
+var firstTrainMinutesInput = $("#first-train-minutes");
+var frequencyInput = $("#frequency-input");
+var addTrainBtn = $("#add-train-btn");
+
+// connect the sign-in and out buttons
+$(signInBtn).click(signIn);
+$(signOutBtn).click(signOut);
+
+$(addTrainBtn).click(onAddTrainBtn);
+
+trainsRef.onSnapshot(updateCurrentTrainSchedule);
+
+setInterval(updateCurrentTrainSchedule, 60000);
